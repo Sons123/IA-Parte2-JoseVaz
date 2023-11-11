cc651b0d98f90ef636719c6fa8d06952c4a0ea3a
Sons123 <100420581+Sons123@users.noreply.github.com>
2023-11-11 17:05:55 +0000

    1.Challenge (O ficheiro que me pediram para descarregar, já tinha o exercício feito)

--- Assets/Scripts/State.cs
+++ Assets/Scripts/State.cs
@@ -11,7 +11,8 @@ public class State {
         PATROL,
         PURSUE,
         ATTACK,
-        SLEEP
+        SLEEP,
+        RUNAWAY
     };
 
     public enum EVENT {
@@ -69,6 +70,14 @@ public class State {
             return true;
         }
 
+        return false;
+    }
+
+    public bool IsPlayerBehind() {
+
+        Vector3 direction = npc.transform.position - player.position;
+        float angle = Vector3.Angle(direction, npc.transform.forward);
+        if (direction.magnitude < 2.0f && angle < 30.0f) return true;
         return false;
     }
 
@@ -169,6 +178,10 @@ public class Patrol : State {
 
             nextState = new Pursue(npc, agent, anim, player);
             stage = EVENT.EXIT;
+        } else if (IsPlayerBehind()) {
+
+            nextState = new RunAway(npc, agent, anim, player);
+            stage = EVENT.EXIT;
         }
     }
 
@@ -261,6 +274,42 @@ public class Attack : State {
     public override void Exit() {
 
         anim.ResetTrigger("isShooting");
+        base.Exit();
+    }
+}
+
+public class RunAway : State {
+
+    GameObject safeLocation;
+
+    public RunAway(GameObject _npc, NavMeshAgent _agent, Animator _anim, Transform _player)
+        : base(_npc, _agent, _anim, _player) {
+
+        name = STATE.RUNAWAY;
+        safeLocation = GameObject.FindGameObjectWithTag("Safe");
+    }
+
+    public override void Enter() {
+
+        anim.SetTrigger("isRunning");
+        agent.isStopped = false;
+        agent.speed = 6;
+        agent.SetDestination(safeLocation.transform.position);
+        base.Enter();
+    }
+
+    public override void Update() {
+
+        if (agent.remainingDistance < 1.0f) {
+
+            nextState = new Idle(npc, agent, anim, player);
+            stage = EVENT.EXIT;
+        }
+    }
+
+    public override void Exit() {
+
+        anim.ResetTrigger("isRunning");
         base.Exit();
     }
 }
