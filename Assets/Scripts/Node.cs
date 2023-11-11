e48b72e26af49130ada2ae3b49cab2e11a46b535
Sons123 <100420581+Sons123@users.noreply.github.com>
2023-11-11 22:58:54 +0000

    Leaf and Action Nodes, NavMesh Movement, Sequences, Selectors, Extended Action Methods, Conditions (O ficheiro que me pediram para descarregar já tinha todas estas atividades feitas)

--- Assets/Scripts/Node.cs
+++ Assets/Scripts/Node.cs
@@ -2,9 +2,15 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Node
-{
-    public enum Status { SUCESS, RUNNING, FAILURE };
+public class Node {
+
+    public enum Status {
+
+        SUCCESS,
+        RUNNING,
+        FAILURE
+    };
+
     public Status status;
     public List<Node> children = new List<Node>();
     public int currentChild = 0;
@@ -12,13 +18,18 @@ public class Node
 
     public Node() { }
 
-    public Node(string n)
-    {
+    public Node(string n) {
+
         name = n;
     }
 
-    public void AddChild(Node n)
-    {
+    public virtual Status Process() {
+
+        return children[currentChild].Process();
+    }
+
+    public void AddChild(Node n) {
+
         children.Add(n);
     }
 }
